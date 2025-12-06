d56400dedba80e12b5376e3cd06a0c850f25c7b8
Bikash sarker <bikash.webdeveloper5@gmail.com>
2025-12-06 11:55:19 +0600

    the add new page design notification page

---
+++ src/app/notification/page.tsx
@@ -0,0 +1,11 @@
+import NotificationForm from "@/components/notification/NotificationForm";
+
+const page = () => {
+  return (
+    <div>
+      <NotificationForm />
+    </div>
+  );
+};
+
+export default page;
