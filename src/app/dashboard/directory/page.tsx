bac9b5f035be996783b2b08e15534b60eafec3fb
Bikash Sarker <bikash.webdeveloper5@gmail.com>
2026-03-25 09:24:46 +0600

    the create new page design employee directory page done

---
+++ src/app/dashboard/directory/page.tsx
@@ -0,0 +1,12 @@
+import EmployeeDirectory from "@/components/dashboard/directoryEmployee/DirectoryEmployee"
+
+
+const page = () => {
+  return (
+    <div>
+        <EmployeeDirectory />
+    </div>
+  )
+}
+
+export default page
