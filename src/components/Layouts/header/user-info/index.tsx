a529908dfdb7ba68e5de3424563776df2a740f65
Bikash Sarker <bikash.webdeveloper5@gmail.com>
2026-03-26 15:56:36 +0600

    the build error fixed

--- src/components/Layouts/header/user-info/index.tsx
+++ src/components/Layouts/header/user-info/index.tsx
@@ -11,9 +11,11 @@ import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 import { LogOutIcon, SettingsIcon, UserIcon } from "./icons";
+import { useRouter } from "next/navigation";
 
 export function UserInfo() {
   const [isOpen, setIsOpen] = useState(false);
+  const router = useRouter();
 
   const USER = {
     name: "John Smith",
