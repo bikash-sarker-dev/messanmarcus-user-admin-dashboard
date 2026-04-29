97352f7d8c641e83e78281b9accb8ec40a93b687
Bikash Sarker <bikash.webdeveloper5@gmail.com>
2026-04-29 08:55:48 +0600

    the loading reactspinner

--- src/components/Loading/LoadingButton.tsx
+++ src/components/Loading/LoadingButton.tsx
@@ -1,3 +1,18 @@
+// "use client";
+
+// import React from "react";
+// import { PulseLoader } from "react-spinners";
+
+// const LoadingButton = () => {
+//   return (
+//     <div className="">
+//       <PulseLoader color="#fff" size={13} speedMultiplier={0.7} />
+//     </div>
+//   );
+// };
+
+// export default LoadingButton;
+
 "use client";
 
 import React from "react";
@@ -5,8 +20,10 @@ import { PulseLoader } from "react-spinners";
 
 const LoadingButton = () => {
   return (
-    <div className="">
-      <PulseLoader color="#fff" size={13} speedMultiplier={0.7} />
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/40 backdrop-blur-sm">
+      <div className="rounded-lg bg-teal-700 px-6 py-4 shadow-lg">
+        <PulseLoader color="#fff" size={10} speedMultiplier={0.6} />
+      </div>
     </div>
   );
 };
