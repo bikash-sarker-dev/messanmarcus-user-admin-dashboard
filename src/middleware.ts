506145e36b790306a0c72fce2ecfbe212a97351d
Bikash Sarker <bikash.webdeveloper5@gmail.com>
2026-05-06 22:24:00 +0600

    the add praivate route

---
+++ src/middleware.ts
@@ -0,0 +1,41 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  const token = request.cookies.get("accessToken")?.value;
+  console.log("re-token", token);
+
+  // ✅ Always allow static files & Next.js internals
+  if (
+    pathname.startsWith("/_next/") ||
+    pathname.startsWith("/favicon.ico") ||
+    /\.(.*)$/.test(pathname)
+  ) {
+    return NextResponse.next();
+  }
+
+  const isLoginPage = pathname === "https://greetely.com/login";
+
+  // 🔒 No token → ONLY /login is allowed, everything else → redirect to login
+  if (!token) {
+    if (isLoginPage) {
+      return NextResponse.next(); // allow login page
+    }
+    return NextResponse.redirect(
+      new URL("https://greetely.com/login", request.url),
+    );
+  }
+
+  // ✅ Has token → block login page, send to home
+  if (token && isLoginPage) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
+};
